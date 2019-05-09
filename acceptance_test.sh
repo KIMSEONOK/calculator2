3a7ef205eb85b59659e403860f71e404822cb523
KIMSEONOK <tjsdhr0320@naver.com>
2019-05-09 14:42:14 +0900

    Asdfasdf

--- acceptance_test.sh
+++ acceptance_test.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+#test $(curl --silent localhost:$CALCULATOR_PORT/sum?a=1\&b=2) -eq3 || \  # true when it runs on host
+#test $(curl --silent $(docker network inspect -f '{{range .IPAM.Config}}{{.Gateway}}{{end}}' bridge):8090/sum?a=1\&b=2) # true on docker
 CALCULATOR_PORT=$(docker-compose port calculator 8090 | cut -d: -f2)
 test $(curl localhost:$CALCULATOR_PORT/sum?a=1\&b=2) -eq3
 
